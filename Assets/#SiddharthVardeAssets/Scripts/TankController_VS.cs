9f24ec323d8bdd3f71cef821325b194cb076b894
SiddharthVarde22 <21stargames@gmail.com>
2023-09-04 20:39:17 +0530

    Added Joystick, Changed Platform to android, Added player movements using Joystick

---
+++ Assets/#SiddharthVardeAssets/Scripts/TankController_VS.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController_VS : MonoBehaviour
+{
+    [SerializeField]
+    Joystick joystickController;
+    [SerializeField]
+    float tankMoveSpeed = 10;
+
+    //To convert 2d Input in 3D.
+    Vector3 inputDirection = Vector3.zero;
+
+    private void Update()
+    {
+        MoveTank();
+    }
+
+    void MoveTank()
+    {
+        //inputDirection.x = joystickController.Direction.x;
+        //inputDirection.y = 0;
+        //inputDirection.z = joystickController.Direction.y;
+
+        //inputDirection = transform.InverseTransformDirection(inputDirection);
+
+        inputDirection = ((transform.right * joystickController.Direction.x) + (transform.forward * joystickController.Direction.y));
+        transform.position += (tankMoveSpeed * Time.deltaTime * inputDirection);
+    }
+}
