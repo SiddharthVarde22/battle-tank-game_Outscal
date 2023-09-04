f9d3f8d802b352f82ae8911ee9e9d5b7c2516a5c
SiddharthVarde22 <21stargames@gmail.com>
2023-09-04 18:15:09 +0530

    Assignment 1, creating Player and Enemy Singleton classed using a generic singleton class

---
+++ Assets/#SiddharthVardeAssets/Scripts/PlayerTank_VS.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank_VS : GenericSingleton_VS<PlayerTank_VS>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
